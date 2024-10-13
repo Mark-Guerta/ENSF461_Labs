06393a481c2141c96b23f01bec1d6f42b980fd3a
Mark Guerta <mark.guerta@ucalgary.ca>
2024-10-13 18:52:46 +0000

    STCF completed

--- lab4/scheduler.c
+++ lab4/scheduler.c
@@ -441,12 +441,22 @@ int main(int argc, char **argv) {
                 double avgWait = 0;
 
                 while(temp){
-                    printf("Job %d -- Response time: %d  Turnaround: %d  Wait: %d\n", temp->id - 1, temp->start_time - temp->arrival, temp->end_time - temp->arrival, temp->start_time - temp->arrival);
-                    avgResponse += (temp->start_time - temp->arrival);
-                    avgTurnaround += (temp->end_time - temp->arrival);
-                    avgWait += (temp->start_time - temp->arrival);
-                    temp = temp->next;
-                    i++;
+                   if (temp->length > temp->end_time - temp->start_time){
+                        printf("Job %d -- Response time: %d  Turnaround: %d  Wait: %d\n", temp->id - 1, 0, temp->end_time - temp->arrival, (temp->end_time - temp->arrival) - temp->length);
+                        avgResponse += 0;
+                        avgTurnaround += (temp->end_time - temp->arrival);
+                        avgWait += (temp->end_time - temp->arrival) - temp->length;
+                        temp = temp->next;
+                        i++;
+                    }
+                   else{
+                        printf("Job %d -- Response time: %d  Turnaround: %d  Wait: %d\n", temp->id - 1, temp->start_time - temp->arrival, temp->end_time - temp->arrival, temp->start_time - temp->arrival);
+                        avgResponse += (temp->start_time - temp->arrival);
+                        avgTurnaround += (temp->end_time - temp->arrival);
+                        avgWait += (temp->start_time - temp->arrival);
+                        temp = temp->next;
+                        i++;
+                   }
                 }
 
                 avgResponse = avgResponse / i;
