53e25da57ec504afef915ebc1746ec92f21048e5
Mark Guerta <mark.guerta@ucalgary.ca>
2024-10-24 19:07:44 +0000

    Lab 5 part 1 done

--- lab5-template/myalloc.c
+++ lab5-template/myalloc.c
@@ -1,30 +1,45 @@
-#include "myalloc.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stddef.h>
 #include <unistd.h>
 #include <sys/mman.h>
-
+#include "myalloc.h"
 void *_arena_start = NULL;
-
-int size_m = 0;
+node_t *header;
+int size_M = 0;
 
 int myinit(size_t size){
-    size_m = size;
-    _arena_start = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0 );
-    return _arena_start;
+    if ((int) size < 0){
+        size_M = ERR_BAD_ARGUMENTS;
+    }
+    else{
+        int multiple = size/4096.0 > size/4096? (size/4096.0 + 1) : size/4096.0;
+        size_M = (multiple)*4096;
+        _arena_start = mmap(NULL, size, PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_ANONYMOUS, -1, 0 );
+    }
+
+    return size_M;
 }
 
 
 int mydestroy(){
-    return munmap(_arena_start, size_m);
+    if (_arena_start == NULL){
+        size_M = ERR_UNINITIALIZED;
+    }
+    else{
+        munmap(_arena_start, size_M);
+        size_M = 0;
+        _arena_start = NULL;
+    }
+    return size_M;
+    
 }
 
 
-void* myalloc(size_t size){
+// void* myalloc(size_t size){
+    
+// }
 
-}
+// void myfree(void *ptr){
 
-void myfree(void *ptr){
-
-}
+// }
