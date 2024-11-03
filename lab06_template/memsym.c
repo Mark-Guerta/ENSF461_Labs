4ec08d3cb2057ce649b47ff480d7c4bbce7e50ba
Mohammed Yassin <mohammed.yassin@ucalgary.ca>
2024-11-03 20:12:02 +0000

    Fixed address translation and TLB miss Program crahes after changing processes

--- lab06_template/memsym.c
+++ lab06_template/memsym.c
@@ -149,22 +149,26 @@ void log_TLB(int m_VPN, int m_PFN){
 
 int lookup_TLB(int VPN){
     for(int i = 0; i < TLB_NUM; i++){
-        if(TLB[i].VPN == VPN){
+        if(TLB[i].VPN == VPN && TLB[i].valid && TLB[i].process == current_process){
             TLB[i].used = time;
+            fprintf(output_file, "Current PID: %d. Translating. Lookup for VPN %d hit in TLB entry %d. PFN is %d\n", current_process, VPN, i, TLB[i].PFN);
+            fflush(output_file);
             return TLB[i].PFN;
         }
     }
     
-    //TLB MISS
+    // TLB MISS
+    fprintf(output_file, "Current PID: %d. Translating. Lookup for VPN %d missed in TLB\n", current_process, VPN);
+    fflush(output_file);
 
     int PFN = lookup_pageTable(VPN);
-
-    if(strcmp("FIFO",strategy) == 0){
-        fifo(VPN, PFN);
-    }else if(strcmp("LRU",strategy) == 0){
-        lru(VPN, PFN);
+    if (PFN == -1) {
+        fprintf(output_file, "Current PID: %d. Error: VPN %d not found in page table\n", current_process, VPN);
+        fflush(output_file);
+        return -1;
     }
 
+    log_TLB(VPN, PFN);
     return PFN;
 }
 
@@ -191,10 +195,12 @@ void handle_Define(int OFF, int PFN, int VPN) {
 void handle_ctxswitch(int pid) {
     if (pid < 0 || pid >= MAX_PROCESSES) {
         fprintf(output_file, "Current PID: %d. Invalid context switch to process %d\n", current_process, pid);
+        fflush(output_file);
         return;
     }
     current_process = pid;
     fprintf(output_file, "Current PID: %d. Switched execution context to process: %d\n", current_process, pid);
+    fflush(output_file);
 }
 
 void handle_map(int m_VPN, int m_PFN){
@@ -205,6 +211,7 @@ void handle_map(int m_VPN, int m_PFN){
             processes[current_process].pageTable[i].PFN = m_PFN;
             log_TLB( m_VPN, m_PFN);
             fprintf(output_file, "Current PID: %d. Mapped virtual page number %d to physical frame number %d\n", current_process, m_VPN, m_PFN);
+            fflush(output_file);
             return;
         }
     }
@@ -215,6 +222,7 @@ void handle_map(int m_VPN, int m_PFN){
             processes[current_process].pageTable[i].PFN = m_PFN;
             log_TLB( m_VPN, m_PFN);
             fprintf(output_file, "Current PID: %d. Mapped virtual page number %d to physical frame number %d\n", current_process, m_VPN, m_PFN);
+            fflush(output_file);
             return;
         }
     }
@@ -241,6 +249,7 @@ void handle_unmap(int m_VPN){
     processes[current_process].pageTable[m_VPN].PFN = -1;
     processes[current_process].pageTable[m_VPN].valid = FALSE;
     fprintf(output_file, "Current PID: %d. Unmapped virtual page number %d\n", current_process, m_VPN);
+    fflush(output_file);
 
     remove_LTB(m_VPN);
     return;
@@ -251,11 +260,13 @@ void handle_pinspect(int m_VPN){
     int vpn = processes[current_process].pageTable[m_VPN].VPN;
     int valid = processes[current_process].pageTable[m_VPN].valid;
     fprintf(output_file, "Current PID: %d. Inspected page table entry %d. Physical frame number: %d. Valid: %d\n", current_process, vpn, pfn, valid);
+    fflush(output_file);
   
 }
 
 void handle_tinspect(int m_TLB){
     fprintf(output_file, "Current PID: %d. Inspected TLB entry %d. VPN: %d. PFN: %d. Valid: %d. PID: %d. Timestamp: %d\n", current_process, m_TLB, TLB[m_TLB].VPN, TLB[m_TLB].PFN, TLB[m_TLB].valid, TLB[m_TLB].process, TLB[m_TLB].time_stamp );
+    fflush(output_file);
 }
 
 int load_from_memory(uint32_t physical_address) {
@@ -310,6 +321,7 @@ int main(int argc, char* argv[]) {
         }else if(strcmp(tokens[0], "define") == 0) {
             if(defineCall == 1){
                 fprintf(output_file,"Current PID: %d. Error: multiple calls to define in the same trace\n", current_process);
+                fflush(output_file);
                 return -1;
             }
             int OFF = atoi(tokens[1]);
@@ -321,6 +333,7 @@ int main(int argc, char* argv[]) {
             processes[current_process].OFF = OFF;
             processes[current_process].PFN = PFN;
             fprintf(output_file, "Current PID: %d. Memory instantiation complete. OFF bits: %d. PFN bits: %d. VPN bits: %d\n", current_process,OFF, PFN, VPN);
+            fflush(output_file);
         }
         else if (strcmp(tokens[0], "ctxswitch") == 0) {
             int pid = atoi(tokens[1]);
@@ -328,6 +341,7 @@ int main(int argc, char* argv[]) {
         }
         else if (defineCall == 0) {
             fprintf(output_file,"Current PID: %d. Error: attempt to execute instruction before define\n", current_process);
+            fflush(output_file);
             return -1;
         }else if (strcmp(tokens[0], "map") == 0) {
             int m_VPN = atoi(tokens[1]);
@@ -347,10 +361,13 @@ int main(int argc, char* argv[]) {
         }else if (strcmp(tokens[0], "rinspect") == 0) {
             if(strcmp("r1", tokens[1]) == 0){
                 fprintf(output_file, "Current PID: %d. Inspected register r1. Content: %d\n", current_process, processes[current_process].R1);
+                fflush(output_file);
             }else if(strcmp(tokens[1], "r2") == 0){
                 fprintf(output_file, "Current PID: %d. Inspected register r2. Content: %d\n", current_process, processes[current_process].R2);
+                fflush(output_file);
             }else{
                 fprintf(output_file, "Current PID: %d. Error: invalid register operand %s\n", current_process, tokens[1]);
+                fflush(output_file);
             }
         }else if (strcmp(tokens[0], "load") == 0) {
             if (tokens[2][0] == '#') {
@@ -360,14 +377,15 @@ int main(int argc, char* argv[]) {
                 if (strcmp(tokens[1], "r1") == 0){
                     processes[current_process].R1 = immediate_value;
                     fprintf(output_file, "Current PID: %d. Loaded immediate %d into register r1\n", current_process, immediate_value);
+                    fflush(output_file);
                 }else if(strcmp(tokens[1], "r2") == 0){
                     processes[current_process].R2 = immediate_value;
                     fprintf(output_file, "Current PID: %d. Loaded immediate %d into register r2\n", current_process, immediate_value);
+                    fflush(output_file);
                 }else{
                     fprintf(output_file, "Current PID: %d. Error: invalid register operand %s\n", current_process, tokens[1]);
-
+                    fflush(output_file);
                 }
-                
             } else {
                 // Memory load
                 int virtual_address = atoi(tokens[2]);
@@ -376,6 +394,11 @@ int main(int argc, char* argv[]) {
 
                 // Find the PFN for the given VPN
                 int PFN = lookup_TLB(VPN);
+                if (PFN == -1) {
+                    fprintf(output_file, "Current PID: %d. Error: invalid virtual address %d\n", current_process, virtual_address);
+                    fflush(output_file);
+                    continue;
+                }
 
                 // Calculate the physical address
                 int physical_address = (PFN << processes[current_process].OFF) | offset;
@@ -385,15 +408,16 @@ int main(int argc, char* argv[]) {
 
                 if (strcmp(tokens[1], "r1") == 0){
                     processes[current_process].R1 = value;
-                    fprintf(output_file, "Current PID: %d. Loaded value of location %d (%d) into register r1\n", current_process, VPN,value);
+                    fprintf(output_file, "Current PID: %d. Loaded value of location %d (%d) into register r1\n", current_process, virtual_address, value);
+                    fflush(output_file);
                 }else if(strcmp(tokens[1], "r2") == 0){
                     processes[current_process].R2 = value;
-                    fprintf(output_file, "Current PID: %d. Loaded value of location %d (%d) into register r2\n", current_process, VPN,value);
+                    fprintf(output_file, "Current PID: %d. Loaded value of location %d (%d) into register r2\n", current_process, virtual_address, value);
+                    fflush(output_file);
                 }else{
                     fprintf(output_file, "Current PID: %d. Error: invalid register operand %s\n", current_process, tokens[1]);
-
+                    fflush(output_file);
                 }
-
             }
         }else if (strcmp(tokens[0], "store") == 0) {
             int virtual_address = atoi(tokens[1]);
@@ -402,18 +426,22 @@ int main(int argc, char* argv[]) {
         
             // Find the PFN for the given VPN
             int PFN = lookup_TLB(VPN);
+            if (PFN == -1) {
+                fprintf(output_file, "Current PID: %d. Error: invalid virtual address %d\n", current_process, virtual_address);
+                fflush(output_file);
+                continue;
+            }
         
             // Calculate the physical address
             int physical_address = (PFN << processes[current_process].OFF) | offset;
         
-
             if (tokens[2][0] == '#') {
                 // Immediate value
                 int immediate_value = atoi(tokens[2] + 1); // Skip the '#' character
                 store_to_memory(physical_address, immediate_value);
                 // Use the immediate value directly
                 fprintf(output_file, "Current PID: %d. Stored immediate %d into location %s\n", current_process, immediate_value, tokens[1]);
-                
+                fflush(output_file);
             } else {
                 int s_val;
                 if (strcmp(tokens[2], "r1") == 0){
@@ -422,17 +450,20 @@ int main(int argc, char* argv[]) {
                     s_val = processes[current_process].R2;
                 }else{
                     fprintf(output_file, "Current PID: %d. Error: invalid register operand %s\n", current_process, tokens[1]);
-
+                    fflush(output_file);
+                    continue;
                 }
 
                 store_to_memory(physical_address, s_val);
 
-                fprintf(output_file, "Current PID: %d. Stored value of register %s (%d) into location %d\n", current_process,tokens[2], s_val, VPN);
+                fprintf(output_file, "Current PID: %d. Stored value of register %s (%d) into location %d\n", current_process, tokens[2], s_val, virtual_address);
+                fflush(output_file);
             }
         }else if (strcmp(tokens[0], "add") == 0) {
             int r1 = processes[current_process].R1;
             processes[current_process].R1 += processes[current_process].R2;
             fprintf(output_file, "Current PID: %d. Added contents of registers r1 (%d) and r2 (%d). Result: %d\n", current_process, r1, processes[current_process].R2, processes[current_process].R1);
+            fflush(output_file);
         }
 
 
